1b3351030c82e9fd92e2253508f87501b78633bf
Ben Williams <btw19@duke.edu>
2020-06-08 10:31:14 -0400

    Including Peyton's code for lat/long finding

--- website/utilitypage.py
+++ website/utilitypage.py
@@ -8,6 +8,7 @@ Created on Thu Jun  4 09:38:45 2020
 
 from flask import Flask, request
 from flask import render_template
+from geopy.geocoders import Nominatim
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,20 +24,24 @@ def utiltyfinder():
 @app.route('/search', methods=["GET", "POST"])
 def search():
         if request.method == "POST":
+            lista = []
             address = request.form["address"]
             zipcode = request.form["zipcode"]
             state = request.form["state"]
-            latitude, longitude = geocode(address, zipcode, state)
+            lista.append(address)
+            lista.append(zipcode)
+            lista.append(state)
+            addressfull = ', '.join(lista)
+            latitude, longitude = geocode(addressfull)
 
-
-            return render_template("search.html", lat=latitude, lon=longitude)
+            return render_template("search.html", lat=latitude, lon=longitude, ad=address, zipc=zipcode, st = state)
 
 
 locator = Nominatim(user_agent="otherGeocoder")
 
-def geocode (address, zipcode, state):
+def geocode (addressfull):
     try: 
-        location = locator.geocode(address, zipcode, state)
+        location = locator.geocode(addressfull)
         if location is None:
             return 0, 0
         else:     
