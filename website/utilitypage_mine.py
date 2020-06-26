30570ccbce3827edd03d9d363a96a869c38a2657
Evan Wyse <evan.wyse@duke.edu>
2020-06-26 14:18:24 -0400

    Added graph interactivity

--- website/utilitypage_mine.py
+++ website/utilitypage_mine.py
@@ -6,7 +6,7 @@ Created on Thu Jun  4 09:38:45 2020
 @author: benwilliams
 """
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from flask import render_template
 from geopy.geocoders import Nominatim
 import pandas as pd
@@ -16,6 +16,8 @@ import geopy
 from shapely.geometry import Polygon, Point, MultiPolygon
 import shapefile
 from matplotlib import pyplot as plt
+from sqlalchemy import create_engine
+
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -52,8 +54,11 @@ def search():
             latitude, longitude = geocode(addressfull)
             utility = correct_utility_function(latitude, longitude)
             (utility3, link2) = utility
-            mockGageData = pd.read_csv(r"/Users/benwilliams/Documents/Data+/nwis_data_mock.csv", header=0, names=['date', 'value', 'data_type', 'nwis_site_no'])
-        numberToName = pd.read_csv(r"/Users/benwilliams/Documents/Data+/nwis_site_info.csv")
+            # mockGageData = pd.read_csv(r"/Users/benwilliams/Documents/Data+/nwis_data_mock.csv", header=0, names=['date', 'value', 'data_type', 'nwis_site_no'])
+            folder = 'C:\\Users\\wyseg\\'
+
+            mockGageData = pd.read_csv(os.path.join(folder, 'nwis_data_mock.csv'), header=0, names=['date', 'value', 'data_type', 'nwis_site_no'])
+        numberToName = pd.read_csv(os.path.join(folder, 'nwis_site_info.csv'))
         inputSite = '02043433'
         df2 = mockGageData[mockGageData['nwis_site_no']== inputSite]
         df3 = df2[['date', 'value']]
@@ -67,6 +72,21 @@ def search():
             dataToPlot = sdtp.replace('value', 'precipitation')
         gageTitle = df4['1'][0]
         return render_template("search.html", lat=latitude, lon=longitude, ad=address, zipc=zipcode, st=state, uname=utility3, linkname=link2, dtp = dataToPlot, gtt = gageTitle)
+
+@app.route('/nwis_data/<path:site_no>',methods=['GET'])
+def send_data(site_no):
+    query = """SELECT ts, signal FROM nwis.daily WHERE nwis_site_no = '{}'""".format(site_no)
+    data = pd.read_sql_query(query, cnx)# get site_data from sql 
+    data['ts'] = data['ts'].apply(lambda x: x.strftime("%Y-%m-%d") )
+    # {"dates": [1,2,3,4], "signal": [1,2,3,4]}
+
+    return jsonify(**data.to_dict('split'))
+
+# use these when you need to create/alter tables. DO NOT allow them to be displayed/served on a webpage, or check them into git
+
+
+
+
 
 @app.route('/node_modules/<path:path>')
 def send_js(path):
@@ -100,6 +120,21 @@ def geocode (addressfull):  #geocoder address to coordinates
             return location.latitude, location.longitude
     except:
         return "Address Unclear", "Consider Rewriting"
+
+
+hostname = 'rapid-1304.vm.duke.edu'
+port = '5432'
+username = 'group3_read'
+password = 'water3all4me'
+dbname = 'postgres'
+
+
+postgres_str = 'postgresql://{username}:{password}@{hostname}:{port}/{dbname}'.format(hostname=hostname,
+                                                                                 port=port,
+                                                                                 username=username,
+                                                                                  password=password,
+                                                                                 dbname=dbname)
+cnx = create_engine(postgres_str)
 
 
 response = requests.get('https://www.ncwater.org/Drought_Monitoring/statusReport.php/')  #conservation status info webscraping
@@ -144,7 +179,8 @@ Link_Dataframe.columns = ["External Links"]
 Bigger_Dataframe = pd.merge(Newest_Updates, Link_Dataframe, left_index=True, right_index=True)
 Bigger_Dataframe
 
-StateWide = gpd.read_file(r"/Users/benwilliams/Documents/Data+/ABOUT-US/Boundaries/NC_statewide_CWS_areas.gpkg") #make large usable dataframe with both names and links
+filepath = os.path.join('..','Boundaries', 'NC_statewide_CWS_areas.gpkg')
+StateWide = gpd.read_file(filepath) #make large usable dataframe with both names and links
 StateWide.geometry= StateWide.geometry.to_crs(epsg="4326")
 Combined_Utility = pd.merge(Bigger_Dataframe, StateWide, 'right', on="PWSID")
 
